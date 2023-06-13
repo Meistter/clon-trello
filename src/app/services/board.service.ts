1d2e3e510886c32a715679a45767c2578e904f02
Josue Perez <josueperez723@gmail.com>
2023-06-12 20:55:59 -0400

    calculo de posicion del card

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -12,6 +12,7 @@ import { Card } from '@models/card.model';
 export class BoardService {
 
   apiUrl = environment.API_URL;
+  bufferSpace = 65535
 
   constructor(private http: HttpClient) {}
 // en este tipado le estamos diciendo es que el id use el tipo definido en el id del modelo de Board
@@ -26,16 +27,20 @@ export class BoardService {
     const lastIndex = cards.length - 1
   if (cards.length ===1){
     // Significa que es nueva la card porq esta sola en la lista
-    return 'is new'
+    return this.bufferSpace
   }
   if (cards.length > 1 && currentIndex ===0){
-    return 'is the top'
+    const onTopPosition = cards[1].position // porq posicion 1? porq ahi se ubica el q estaba mas arriba luego de mover la card de lugar
+    return onTopPosition / 2
   }  
   if(cards.length > 2 && currentIndex > 0 && currentIndex < lastIndex){
-    return 'is the middle'
+    const prevPosition = cards[currentIndex - 1].position
+    const nextPosition = cards[currentIndex + 1].position
+    return (prevPosition + nextPosition) / 2
   }
   if(cards.length > 1 && currentIndex == lastIndex){
-    return 'is the bottom'
+    const onBottomPosition = cards[lastIndex - 1].position
+    return onBottomPosition + this.bufferSpace
   } 
   return 'nada'
    
